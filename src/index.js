0eecc88b8561570744581783c2b2ab8feaa3a12e
renox <planetrenox@protonmail.com>
2024-02-06 01:28:34 -0800

    Delete unused index.js and update package dependencies
    The index.js file was deleted as it was no longer necessary. In addition, multiple additions and updates were made in the package.json file. Several new dependencies were added, particularly modules related to web drivers and Puppeteer. The package-lock.json file was also modified to reflect these changes. Changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
+// Filename: openFirefoxWithProfile.js
 
-console.log("Test npm run dev");
-        
-module.exports = {  };
+const {Builder, until} = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+
+const profilePath = 'C:\\Users\\SDS\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\zew5fvgj.default';
+
+let options = new firefox.Options();
+options.setProfile(profilePath);
+
+
+(async function openYoutubeChannel()
+{
+    let driver = await new Builder()
+    .forBrowser('firefox')
+    .setFirefoxOptions(options)
+    .build();
+
+    try
+    {
+        await driver.get('https://www.youtube.com/@planetrenox');
+        await driver.wait(until.urlContains("not"), 10000000);
+    }
+    finally
+    {
+        //await driver.quit();
+    }
+})();
+
+
+module.exports = {};
